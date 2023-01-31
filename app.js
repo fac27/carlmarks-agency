3d33bd8185a949317c5e5cfd036b02db134ca5b7
Mark hanley <home@markhanley.co.uk>
2023-01-31 12:12:04 +0000

    Set up page themes, add to accessibility menu, create JS functions
    Relates 29

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 let navMenuDisplay = document.getElementById("hamburger-nav");
 
 function hamburgerToggle() {
@@ -8,3 +7,29 @@ function hamburgerToggle() {
 function closeMenuToggle() {
   navMenuDisplay.style.display = "none";
 };
+
+function changeTheme(displayChange) {
+  const bodyElement = document.getElementsByTagName("body")[0];
+  bodyElement.setAttribute("data-theme", displayChange);
+}
+
+function changeFontSize(action) {
+  // Set minumum font size constant to 10px
+  const MIN_FONT_SIZE = 10;
+  const rootElement = document.documentElement;
+
+  const fontSizeString = window.getComputedStyle(rootElement, null).getPropertyValue('font-size');
+  
+  fontSize = parseFloat(fontSizeString);
+  let baseFontSize = Math.floor(fontSize);
+
+  if (action == "increase") {
+    baseFontSize++;
+  }
+
+  if (action == "decrease" && fontSize >= MIN_FONT_SIZE) {
+    baseFontSize--;
+  }
+  
+  rootElement.style.fontSize=`${baseFontSize}px`;
+}
